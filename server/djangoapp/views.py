1d6d666c229d4403c6f30dc6bbf5cd04360b43ec
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2023-06-03 01:01:06 +0200

    views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,16 +27,53 @@ def about(request):
 # Create a `contact` view to return a static contact page
 def contact(request):
     return render(request, 'djangoapp/contact.html')
+
+
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # Handle invalid login credentials
+            return render(request, 'djangoapp/login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'djangoapp/login.html')
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 # ...
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
+def signup(request):
+    if request.method == 'POST':
+        # Get the form data from the request
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Create a new user object
+        user = User.objects.create_user(username=username, password=password,
+                                        first_name=first_name, last_name=last_name)
+
+        # Log in the user
+        login(request, user)
+
+        # Redirect to the home page or any other page
+        return redirect('djangoapp:index')
+
+    # If the request method is GET, render the signup form template
+    return render(request, 'djangoapp/registration.html')
+
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
@@ -53,6 +90,5 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
 
 
