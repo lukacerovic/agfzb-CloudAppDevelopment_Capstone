75aebebecbc95ecdf59ab584b1cca984ff81d2f8
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2023-06-03 21:30:03 +0200

    models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,3 +23,39 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+class DealerReview(models.Model):
+    dealership = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    purchase = models.CharField(max_length=100)
+    review = models.TextField()
+    purchase_date = models.DateField()
+    car_make = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_year = models.IntegerField()
+    sentiment = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
